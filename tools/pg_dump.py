c08231489541067e16faaae6e340711077ad3cbe
Meng-Gen Tsai <plover@gmail.com>
2012-12-13 15:47:30 +0800

    Enhance pg_dump tool for any platform

--- tools/pg_dump.py
+++ tools/pg_dump.py
@@ -1,32 +1,15 @@
+# coding: utf8
+
 import os
 import sys
-
-def pg_dump_win():
-    PG_DUMP = 'pg_dump' # Add PostgreSQL bin directory to the environment variables `PATH`
-    SQL_SCRIPT_FILE = '../db/postgres/pg_dump.sql'
-    CMDLINE = '''{pg_dump} --host=localhost --port=5432 --username=postgres --password --role=stocktotal --file="{file}" --schema-only stocktotal'''.format(
-        pg_dump=PG_DUMP,
-        file=SQL_SCRIPT_FILE
-    )
-    print(CMDLINE)
-    os.system(CMDLINE)
-
-    
-    
-def pg_dump_mac():
-    raise NotImplementedError
-
-    
     
 def main():
-    import platform
-    curr_platform = platform.system()
-    if curr_platform == 'Windows':
-        pg_dump_win()
-    elif curr_platform == 'Darwin':
-        pg_dump_mac()
-    else:
-        raise Exception('Please add support for your platform')
+    SQL_SCRIPT_FILE = '../db/postgres/pg_dump.sql'
+    # In Windows platform, please add PostgreSQL bin directory to the environment variables `PATH`
+    CMDLINE = '''pg_dump --host=localhost --port=5432 --username=postgres --password --role=stocktotal --file="{file}" --schema-only stocktotal'''.format(
+        file=SQL_SCRIPT_FILE
+    )
+    os.system(CMDLINE)
         
 
 
