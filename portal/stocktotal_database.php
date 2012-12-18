8f28db805ca89cc68de5489b5cb604a3d8fe4a65
Meng-Gen Tsai <plover@gmail.com>
2012-12-18 19:29:42 +0800

    Init feature/portal

---
+++ portal/stocktotal_database.php
@@ -0,0 +1,24 @@
+<?php
+    include 'stocktotal_postgres.php';
+    
+    class StocktotalDatabase
+    {
+        public function __construct($db_type) {
+            if ((strcasecmp($db_type, "postgres") == 0) or (strcasecmp($db_type, "postgresql") == 0)) {
+                $this->db_impl = new StocktotalPostgres;
+            }
+            else {
+                throw new Exception('Please add support for your database type');
+            }
+        }
+
+        public function __destruct() {
+        }
+
+        public function query_roe($stock_code) {
+            return $this->db_impl->query_roe($stock_code);
+        }
+        
+        private $db_impl;
+    }
+?>
